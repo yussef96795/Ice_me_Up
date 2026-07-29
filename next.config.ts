b1507576f29a17e7f2b8fc59d543c276da22ab25
yussef96795 <yhsanaoui@gmail.com>
2026-07-29 19:55:02 +0100

    Add gh-pages deployment setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import bundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/Ice_me_Up",
+  trailingSlash: true,
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
